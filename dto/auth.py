19b40c53ef54f2acb18489c58af87d72069d55ee
umatbro <umatbroo@gmail.com>
2022-04-19 21:29:59 +0200

    Clean up unused code

--- dto/auth.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from pydantic import SecretStr
-from pydantic import constr
-
-
-class CreateUserRequest(BaseModel):
-    username: constr(max_length=255)
-    password: SecretStr
--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
 
-from dto.auth import CreateUserRequest
 from schema import graphql_app
 
 app = FastAPI()
 app.include_router(graphql_app)
-
-
-@app.post("/user")
-def create_user(create_user_request: CreateUserRequest):
-    pass
 
 
 if __name__ == "__main__":
